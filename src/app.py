110cc413086aa227f2064fdccd65ea5fd4f2cdd2
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2024-01-15 19:27:23 +0000

    login almost finished

--- src/app.py
+++ src/app.py
@@ -6,18 +6,27 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -66,6 +75,30 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
+
+@app.route('/login', methods=['POST'])
+def login():
+    body = request.get_json(silent=True)
+    if body is None: 
+        return jsonify({'msg': 'You must send data in your body'}), 400
+    if 'email' not in body:
+        return jsonify({'msg': 'Email field is obligatory'}), 400
+    if 'password' not in body:
+        return jsonify({'msg': 'Password field is obligatory'}), 400
+    user = User.query.filter_by(email= body['email']).first()
+    if user is None: 
+        return jsonify({'msg': 'Bad Email or Password'}), 400
+    if user.password != body['password']: 
+        return jsonify({'msg': 'Bad Email or Password'}), 400
+    access_token = create_access_token(identity=user.email)
+    return jsonify({'msg': 'Ok', 'token': access_token})
+
+
+
+
+
+
+
 
 
 # this only runs if `$ python src/main.py` is executed
